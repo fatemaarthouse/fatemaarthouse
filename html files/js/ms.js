e8344cbcada489f2ba647e6677f24d4972fcb4ee
fatemaarthouse <71883473+fatemaarthouse@users.noreply.github.com>
2020-10-05 17:57:51 +0600

    fatema

---
+++ html files/js/ms.js
@@ -0,0 +1,13 @@
+document.getElementById("clickbtn").addEventListener("click", dbblclick);
+function dbblclick(){
+	document.getElementById("demo1").setAttribute("style","opacity:1;transform:scaleY(1)");
+}
+	
+	
+	
+	
+	document.getElementById("clickbtn2").addEventListener("click", dbblclick2);
+function dbblclick2(){
+	document.getElementById("demo1").setAttribute("style","opacity:0;transform:scaleY(0)");
+}
+	
